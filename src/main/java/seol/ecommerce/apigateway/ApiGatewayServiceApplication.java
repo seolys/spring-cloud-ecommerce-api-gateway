f42832c0d3b16b1ece9190d7f481a6311633d056
seol <seolnavy@naver.com>
2021-05-05 22:21:04 +0900

    config서버 연동.

--- src/main/java/seol/ecommerce/apigateway/ApiGatewayServiceApplication.java
+++ src/main/java/seol/ecommerce/apigateway/ApiGatewayServiceApplication.java
@@ -1,13 +1,21 @@
 package seol.ecommerce.apigateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	public HttpTraceRepository httpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
 	}
 
 }
